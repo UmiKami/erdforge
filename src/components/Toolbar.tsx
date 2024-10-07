86eded378e7c713199b50c16971739c033b71257
UmiKami <ggoku02@gmail.com>
2024-10-07 17:59:47 -0400

    React flow setup

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,15 @@
+
+const Toolbar = () => {
+  return (
+    <div className='w-screen bg-neutral-800 text-white px-40'>
+          <ul className='list-none flex gap-6'>
+            <li>Single Select</li>
+            <li>Multi Select</li>
+            <span>|</span>
+            <li>Insert Table</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Toolbar
