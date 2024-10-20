cfd6db929ff10e34f8fd046622cc210d431a85dc
UmiKami <ggoku02@gmail.com>
2024-10-20 18:16:40 -0400

    added strict mode while in dev

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import Layout from './Layout.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
+  <StrictMode>
   <Provider store={store}>
     <Layout />
   </Provider>,
+  </StrictMode>
 )
